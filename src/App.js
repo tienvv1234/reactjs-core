dfc26f5ba668b9e66eae01d4c82fd372b11a5f42
tienvv1234 <tienvv1234@gmail.com>
2022-12-03 18:15:07 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  console.log("App component is rendered");
+  const [counter, setCounter] = useState(1);
+  console.log("counter", counter);
+  const increment = () => {
+    console.log("increment", counter);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // counter will be plus only one and it will be rendered only once
+    
+    
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    // prevCounter is the previous value of counter (1) (4) (7)
+    // useState callback function
+    // counter will be plus three and it will be rendered only once
+    console.log("after increment", counter);
+  };
+  console.log('end of App component');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log("rendering div", counter)}
+      <h1>{counter}</h1>
+      <button onClick={increment}>Increment</button>
     </div>
   );
 }
 
 export default App;
+
