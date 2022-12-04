da563a98c870592b51de4d3f027fb462baa068e7
tienvv1234 <tienvv1234@gmail.com>
2022-12-04 09:02:27 +0700

    case box

--- src/App.js
+++ src/App.js
@@ -13,7 +13,25 @@ const courses = [
 
 function App() {
 
-  const [checked, setChecked] = useState();
+  const [checked, setChecked] = useState([]);
+  const handleCheck = (id) => {
+    //way 1
+    if (checked.includes(id)) {
+      setChecked(checked.filter((item) => item !== id));
+    } else {
+      setChecked([...checked, id]);
+    }
+
+    //way 2
+    // setChecked(prev => {
+    //   if (prev.includes(id)) {
+    //     return prev.filter(item => item !== id);
+    //   }
+    //   return [...prev, id];
+    // });
+  }
+  console.log(checked);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(checked);
@@ -24,7 +42,9 @@ function App() {
         // key is required for React to know which element to update
         // must send key in the root element
         <div key={course.id}>
-          <input type='radio' checked={checked === course.id} onChange={() => setChecked(course.id)}/>
+          <input type='checkbox'
+          checked={checked.includes(course.id)}
+          onChange={() => handleCheck(course.id)}/>
           {course.name}
         </div>
       ))}
