bfcd6fb412410e1ddc8df51f8df71a651d27fc46
tienvv1234 <tienvv1234@gmail.com>
2022-12-07 00:59:57 +0700

    useEffect with interval and setTime

--- src/Content.js
+++ src/Content.js
@@ -24,75 +24,34 @@ import { useState, useEffect } from 'react';
 // 1. callback always run after render (component mounted)
 // 2. Cleanup function always called bufore component unmounted
 
-const tabs = ['posts', 'comments', 'albums'];
 
 function Content() {
-    const [title, setTitle] = useState('');
-    const [posts, setPosts] = useState([]);
-    const [type, setType] = useState('posts');
-    const [showGotop, setShowGotop] = useState(false);
+    const [count, setCount] = useState(180);
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${type} `)
-            .then((res) => res.json())
-            .then((posts) => {
-                setPosts(posts);
-            });
-    }, [type]);
+        console.log('count1', count)
+        const timeId = setInterval(() => {
+            console.log('count', count)
+            // setCount(count - 1);
+            // this will not affect the count
+            // because the count is not changed
+            // variable count is closure
+            setCount(count => count - 1)
+        }, 1000);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            console.log('scroll', window.scrollY);
-            if (window.scrollY > 200) {
-                setShowGotop(true);
-            } else {
-                setShowGotop(false);
-            }
-
-            // setShowGotop(window.scrollY > 200);
-        }
-
-        window.addEventListener('scroll', handleScroll);
-
-        // cleanup
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            clearInterval(timeId);
         }
+    }, []);
 
-    }, [showGotop]);
-
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setCount(count - 1);
+    //     }, 1000);
+    // }, [count]);
     return (
         <div>
-            {tabs.map((tab) => (
-                <button 
-                    key={tab} 
-                    onClick={() => setType(tab)
-                }>
-                    {tab}
-                </button>
-            ))}
-            <input
-                type='text'
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>{post.title || post.name}</li>
-                ))}
-            </ul>
-
-            {showGotop && (
-                <button
-                    style={{
-                        position: 'fixed',
-                        bottom: 20,
-                        right: 20,
-                    }}
-                >
-                    Go to top
-                </button>
-            )}
+            <h1>{count}</h1>
         </div>
     );
 }
